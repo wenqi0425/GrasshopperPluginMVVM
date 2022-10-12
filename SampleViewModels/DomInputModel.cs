d9a707da9fc86d5727ace79c893e6eb46d40c6c7
wenqi0425 <xiaoc288@gmail.com>
2022-10-12 23:14:28 +0200

    DomInputModel

---
+++ SampleViewModels/DomInputModel.cs
@@ -0,0 +1,26 @@
+﻿using SampleMVVM;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleViewModels
+{
+    public class DomInputModel : BaseDomInputModel
+    {
+        private List<DomInputModel> _domInputModels;
+        public List<string> InputValues 
+            => _domInputModels?.Select(s => s.value).ToList();
+        
+        public List<string> InputIds 
+            => _domInputModels?.Select(s => s.id).ToList();
+
+        public List<string> InputNames 
+            => _domInputModels?.Select(s => s.name).ToList();
+
+        public List<string> InputTypes 
+            => _domInputModels?.Select(s => s.type).ToList();
+    }
+}
---
+++ SampleWebview2/RhinoPluginWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SampleWebview2
+{
+    /// <summary>
+    /// Interaction logic for RhinoPluginWindow.xaml
+    /// </summary>
+    public partial class RhinoPluginWindow : Window
+    {
+        public RhinoPluginWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
