b1a81f93c6fd46769a622067e19f38aa164f8c03
wenqi0425 <xiaoc288@gmail.com>
2022-10-12 19:33:03 +0200

    BaseViewModel

--- SampleViewModels/MainViewModel.cs
+++ SampleViewModels/MainViewModel.cs
@@ -9,9 +9,18 @@ using System.Windows.Input;
 
 namespace SampleViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : BaseViewModel
     {
-        public int Counter { get; set; }
+        private int _counter;
+        public int Counter 
+        {
+            get{ return _counter; }
+            set
+            {
+                _counter = value;
+                this.RaisePropertyChanged(nameof(this.Counter));                
+            } 
+        }
         public ICommand CountUpCommand { get; set; }
         public MainViewModel()
         {
