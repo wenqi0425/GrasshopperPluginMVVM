b7195cf59922b4289e1c735c25fb15615c9e6a3d
wenqi0425 <xiaoc288@gmail.com>
2022-10-12 19:09:45 +0200

    Binding CountUpCommand

--- SampleViewModels/MainViewModel.cs
+++ SampleViewModels/MainViewModel.cs
@@ -1,17 +1,22 @@
-﻿using System;
+﻿using SampleMVVM;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SampleViewModels
 {
     public class MainViewModel
     {
         public int Counter { get; set; }
+        public ICommand CountUpCommand { get; set; }
         public MainViewModel()
         {
-            this.Counter = 77;
+            this.Counter = 0;
+            this.CountUpCommand = new RelayCommand(this.CountUp);
         }
 
         public void CountUp()
